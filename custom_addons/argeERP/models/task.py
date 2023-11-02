0eb05e37262c2d2db6570211c31356e7d69fcad8
Eda Dural <edaadurall@gmail.com>
2023-11-02 16:35:27 +0300

    rec name

--- custom_addons/argeERP/models/task.py
+++ custom_addons/argeERP/models/task.py
@@ -4,6 +4,7 @@ class ArgeErpTask(models.Model):
     _name = "argeerp.task"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "ArgeERP Task"
+    _rec_name = "employee_id"
 
     employee_id = fields.Many2one('argeerp.employee', string='Employee', tracking=True)
     employee_task = fields.Char(string='Task', tracking=True)
